3ae69573400233ae2004e8c86926161495755511
Manfred Veiga <2021321227@ifam.edu.br>
2023-11-30 16:16:18 -0400

    colição e derrota

--- script/sketch.js
+++ script/sketch.js
@@ -3,7 +3,7 @@ var s, scala = 20, food;
 //desenhar um quadrado
 function setup() {
 
-  createCanvas(600, 600)
+  createCanvas(1200, 900)
   s = new Snake() //chamar o objeto
   frameRate(10)
   pickLocation(); //criar uma posiçao aleatoria
@@ -14,6 +14,8 @@ function setup() {
 function draw() {
 
   background(51)
+
+  s.death() //funcao de morte
   s.update() //atualizar a posição
   s.show() //mostrar na tela
 
@@ -39,21 +41,98 @@ function Snake() {
   this.yspeed = 0;
 
 
+  //array de controle do tamanho
+  this.total = 0;
+  this.tail = [];
+
+
+
+  //funcao de morte da cobrinha
+  this.death = function(){
+
+    for(let i = 0; i < this.tail.length; i++){
+
+      let pos = this.tail[i];
+      let d = dist(this.x, this.y, pos.x, pos.y)
+
+
+      if(d < 1){
+
+        alert("Você morreu!")
+        this.total = 0;
+        this.tail = []
+        this.x = Math.floor(Math.random() * 599)
+        this.y= Math.floor(Math.random() * 599)
+      }
+
+
+    }
+
+
+  }
+
+
   //atualização da posição da cobra
-  this.update = function () {
+  this.update = function() {
+    for (let i = 0; i < this.tail.length - 1; i++) {
+      this.tail[i] = this.tail[i + 1];
+    }
+    if (this.total >= 1) {
+      this.tail[this.total - 1] = createVector(this.x, this.y);
+    }
 
     this.x = this.x + this.xspeed * scala;
     this.y = this.y + this.yspeed * scala;
 
-
     this.x = constrain(this.x, 0, width - scala);
     this.y = constrain(this.y, 0, height - scala);
-  }
+  };
+
+
+  // this.update = function () {
+
+
+  //   if(this.total === this.tail.lenght){
+
+  //     for(var i = 0; i < this.tail.lenght -1; i++){
+        
+  //       this.tail[i] = this.tail[i+1];
+  //     }
+  //   }
+    
+  //     this.tail[this.total -1] = createVector(this.x, this.y)
+    
+    
+  //   //incrementa o array
+  //   for(var i = 0; i< this.total-1; i++){
+
+  //     this.tail[i] = this.tail[i+1]
+  //   }
+  //   this.tail[this.total -1] = createVector(this.x, this.y)
+  //   this.x = this.x + this.xspeed * scala;
+  //   this.y = this.y + this.yspeed * scala;
+
+  //   this.x = constrain(this.x, 0, width - scala);
+  //   this.y = constrain(this.y, 0, height - scala);
+
+  // }
 
   //mostrar a cobrinha no canvas
   this.show = function () {
 
+  
     fill(255); //definir a cor
+    // for(var i = 0; i < this.tail.lenght; i++){
+        
+    //   this.tail[i] = this.tail[i+1];
+    // }
+
+
+    for(let i = 0;i < this.total; i++){
+
+      rect(this.tail[i].x, this.tail[i].y, scala, scala);
+
+    }
     rect(this.x, this.y, scala, scala)  //desenhar um retângulo
   }
 
@@ -69,6 +148,9 @@ function Snake() {
     var d = dist(this.x, this.y, pos.x, pos.y);
 
     if (d < 1) {
+
+
+      this.total++;
 
       return true
     } else {
